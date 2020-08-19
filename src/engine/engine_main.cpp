91c440744205c50dce5a82a1fe7e648e806e562a
shiro <shironecko.mail@gmail.com>
2020-08-19 17:54:17 +0300

    fix not calling SDL_Quit on shutdown

--- src/engine/engine_main.cpp
+++ src/engine/engine_main.cpp
@@ -6,7 +6,8 @@ int main(int argc, char** argv)
 {
     SDL_Init(SDL_INIT_EVERYTHING);
 
-    return GameMain();
+    int gameReturnCode = GameMain();
 
     SDL_Quit();
+    return gameReturnCode;
 }
