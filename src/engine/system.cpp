f7af87c22b9368d440ab8c1cc34f725529ce5f29
shiro <shironecko.mail@gmail.com>
2020-08-20 16:10:53 +0300

    clean up panic related code a bit

---
+++ src/engine/system.cpp
@@ -0,0 +1,34 @@
+#include <engine/pch.h>
+
+#include <engine/system.h>
+
+namespace cgt
+{
+
+PanicDialogResponse ShowPanicDialog(const char* message, const char* title)
+{
+    SDL_MessageBoxButtonData buttons[] =
+    {
+            { SDL_MESSAGEBOX_BUTTON_RETURNKEY_DEFAULT, 0, "Debug Break" },
+            { SDL_MESSAGEBOX_BUTTON_ESCAPEKEY_DEFAULT, -1, "Quit" }
+    };
+    SDL_MessageBoxData data {};
+    data.buttons = buttons;
+    data.numbuttons = SDL_arraysize(buttons);
+    data.flags = SDL_MESSAGEBOX_ERROR;
+    data.title = title;
+    data.message = message;
+
+    i32 pressedButton = -1;
+    i32 result = SDL_ShowMessageBox(&data, &pressedButton);
+    if (result == -1 || pressedButton == 0)
+    {
+        return PanicDialogResponse::DebugBreak;
+    }
+    else
+    {
+        return PanicDialogResponse::Quit;
+    }
+}
+
+}
--- src/engine/system.h
+++ src/engine/system.h
@@ -10,25 +10,37 @@
 #include <fstream>
 #include <bitset>
 
+typedef int8_t i8;
+typedef int16_t i16;
+typedef int32_t i32;
+typedef int64_t i64;
+
+typedef uint8_t u8;
+typedef uint16_t u16;
+typedef uint32_t u32;
+typedef uint64_t u64;
+
+typedef size_t usize;
+typedef uintptr_t uptr;
+typedef intptr_t ptrdiff;
+
+namespace cgt
+{
+
+enum class PanicDialogResponse
+{
+    DebugBreak,
+    Quit,
+};
+
+PanicDialogResponse ShowPanicDialog(const char* message, const char* title = "Panic!");
+
 #define CGT_PANIC(fmtStr, ...)                                                                                      \
 do {                                                                                                                \
     std::string CGT_PANIC_userMsg = fmt::format(fmtStr, ##__VA_ARGS__);                                             \
     std::string CGT_PANIC_fullMsg = fmt::format("File: {}\nLine: {}\n{}", __FILE__, __LINE__, CGT_PANIC_userMsg);   \
-    SDL_MessageBoxButtonData CGT_PANIC_buttons[] =                                                                  \
-    {                                                                                                               \
-            { SDL_MESSAGEBOX_BUTTON_RETURNKEY_DEFAULT, 0, "Debug Break" },                                          \
-            { SDL_MESSAGEBOX_BUTTON_ESCAPEKEY_DEFAULT, -1, "Quit" }                                                 \
-    };                                                                                                              \
-    SDL_MessageBoxData CGT_PANIC_data {};                                                                           \
-    CGT_PANIC_data.buttons = CGT_PANIC_buttons;                                                                     \
-    CGT_PANIC_data.numbuttons = 2;                                                                                  \
-    CGT_PANIC_data.flags = SDL_MESSAGEBOX_ERROR;                                                                    \
-    CGT_PANIC_data.title = "Panic!";                                                                                \
-    CGT_PANIC_data.message = CGT_PANIC_fullMsg.c_str();                                                             \
-                                                                                                                    \
-    int CGT_PANIC_pressedButton = -1;                                                                               \
-    int CGT_PANIC_result = SDL_ShowMessageBox(&CGT_PANIC_data, &CGT_PANIC_pressedButton);                           \
-    if (CGT_PANIC_result == -1 || CGT_PANIC_pressedButton == 0)                                                     \
+    auto CGT_PANIC_userResponse = ::cgt::ShowPanicDialog(CGT_PANIC_fullMsg.c_str());                   \
+    if (CGT_PANIC_userResponse == ::cgt::PanicDialogResponse::DebugBreak)                                           \
     {                                                                                                               \
         SDL_TriggerBreakpoint();                                                                                    \
     }                                                                                                               \
@@ -50,26 +62,12 @@ while (!(condition))                                                            
 #if defined(DEBUG) || defined(_DEBUG)
 #define CGT_DEBUG_ONLY(stmt) (stmt)
 #define CGT_ASSERT(condition) CGT_ASSERT_ALWAYS((condition))
-#define CGT_ASSERT_MSG(condition, fmtStr, ...) CGT_ASSERT_ALWAYS_MSG((condition), fmtStr, ##__VA_ARGS__)
+#define CGT_ASSERT_MSG(condition, fmtStr, ...) CGT_ASSERT_ALWAYS_MSG((condition), (fmtStr), ##__VA_ARGS__)
 #else
 #define CGT_DEBUG_ONLY(stmt)
 #define CGT_ASSERT(condition)
 #define CGT_ASSERT_MSG(condition, fmtStr, ...)
 #endif
-
-typedef int8_t i8;
-typedef int16_t i16;
-typedef int32_t i32;
-typedef int64_t i64;
-
-typedef uint8_t u8;
-typedef uint16_t u16;
-typedef uint32_t u32;
-typedef uint64_t u64;
-
-typedef size_t usize;
-typedef uintptr_t uptr;
-typedef intptr_t ptrdiff;
 
 class NonCopyable
 {
@@ -83,3 +81,5 @@ public:
 
     NonCopyable& operator=(const NonCopyable&) = delete;
 };
+
+}
